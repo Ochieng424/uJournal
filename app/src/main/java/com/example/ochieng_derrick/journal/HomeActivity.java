459ab4f0cc0e48fa0ddcab83165744b91d6b8a69
Ochieng424 <ochiengderrick424@gmail.com>
2018-07-01 01:26:07 +0300

    Menu with Google sign out complete

--- app/src/main/java/com/example/ochieng_derrick/journal/HomeActivity.java
+++ app/src/main/java/com/example/ochieng_derrick/journal/HomeActivity.java
@@ -1,13 +1,65 @@
 package com.example.ochieng_derrick.journal;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    FloatingActionButton fabAdd;
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mAuth.addAuthStateListener(mAuthListener);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fabAdd = findViewById(R.id.fabAdd);
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                }
+            }
+        };
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, AddActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemSelected = item.getItemId();
+        if (menuItemSelected == R.id.logOut){
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(HomeActivity.this, MainActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/ochieng_derrick/journal/data/JournalContract.java
@@ -0,0 +1,4 @@
+package com.example.ochieng_derrick.journal.data;
+
+public class JournalContract {
+}
